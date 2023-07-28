ed6a6dd2ed6f742f750ec1d4fa0dc58832cb9b1b
Abdullah foysal <abdullah32@student.sust.edu>
2023-07-28 19:55:01 +0600

    sleep data fetch and save

--- HealthManager.swift
+++ HealthManager.swift
@@ -13,12 +13,20 @@ class HealthManager {
     let allTypes = Set([HKObjectType.workoutType(),
                        HKObjectType.quantityType(forIdentifier: .heartRate)!,
                        HKObjectType.quantityType(forIdentifier: .bloodGlucose)!,
-                       HKObjectType.quantityType(forIdentifier: .oxygenSaturation)!])
+                       HKObjectType.quantityType(forIdentifier: .oxygenSaturation)!,
+                        
+                       HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!
+                       ])
+    
+   
+    
     
     func isHealthDataAvailable() -> Bool {
         return HKHealthStore.isHealthDataAvailable()
     }
   
+
+    
     func requestAuthorizationPermission() {
         healthStore.requestAuthorization(toShare: allTypes, read: allTypes) { (success, error) in
             if !success {
@@ -27,8 +35,12 @@ class HealthManager {
                 print(error)
             }else {
                 print("success\(success)")
-                self.readOxygenSaturationData()
-               // self.saveOxygenSaturationData(saturationValue: 100, date: Date)
+               
+
+                self.readSleepData()
+                
+                
+               
             }
         }
     }
@@ -138,7 +150,63 @@ class HealthManager {
         }
     }
 
+    
+   
+   
+    
+    func saveSleepData(startData: Date, endDate: Date,sleepType: HKCategoryValueSleepAnalysis) {
+        
+      
+        
+        guard let sleepTypeSample = HKCategoryType.categoryType(forIdentifier: .sleepAnalysis) else { return }
+        
+        let sleepSample = HKCategorySample(type: sleepTypeSample, value: sleepType.rawValue , start: startData, end: endDate)
+        
+        healthStore.save(sleepSample) { success, error in
+            if success {
+                print("Sleep data saved successfully")
+            }else {
+                print("Error saving sleep data: \(error?.localizedDescription ?? "Unknown error")")
+            }
+        }
+    }
+    
+    
+    func readSleepData() {
+        let sleepType = HKObjectType.categoryType(forIdentifier: .sleepAnalysis)!
+        
+        let query = HKSampleQuery(sampleType: sleepType, predicate: nil, limit: HKObjectQueryNoLimit, sortDescriptors: nil) { query, samples , error in
+            
+            if let error = error {
+                print("Error quering sleep samples: \(error.localizedDescription ?? "unknown")")
+                return
+            }
+            
+            if let sleepSamples = samples as? [HKCategorySample] {
+                for sample in sleepSamples {
+                    print(sample)
+                }
+            }
+            
+            
+            
+        }
+        
+        healthStore.execute(query)
+    }
+    
+    fileprivate func dataFormatter() -> Date {
+        let dateString = "2023-07-28 12:30:36 +0600"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss Z"
+        
+        if let date = dateFormatter.date(from: dateString) {
+         return date
 
+        } else {
+            return Date()
+        }
+    }
     
     
 }
